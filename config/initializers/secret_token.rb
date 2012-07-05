33b3ee69220e9e69023e85a2f74a1236e2f95124
oron lavi <oronlavi@gmail.com>
2012-07-05 14:07:15 +0300

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CbvwPoc::Application.config.secret_token = '70dc7778b9458ce018b9a6d16f7ebe558af38be6cab3229926b5f4e9f3e3abb58ae990a63f6a353efeab94795ca7e5217bf8188358bc1ff45822ecc299e47ed1'
