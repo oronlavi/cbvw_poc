d7d84b491a513e6d7b6033d005eb152f7324e54a
oronlavi <oronlavi@gmail.com>
2012-07-06 15:47:30 +0300

    adds static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 CbvwPoc::Application.routes.draw do
+  get "static_pages/home"
+
+  get "static_pages/help"
+
   resources :matches
   resources :users
 
@@ -53,7 +57,7 @@ CbvwPoc::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-   root :to => 'home#index'
+   root :to => 'matches#index'
 
   # See how all your routes lay out with "rake routes"
 
