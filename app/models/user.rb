f5975fd079c2771d20711cc9943b3029ab5b91a4
oronlavi <oronlavi@gmail.com>
2012-07-10 08:43:02 +0300

    buttonify table actions. case validation on user info

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   attr_accessible :age, :country, :email, :teamname, :username
-  validates :email, :username, :teamname, :uniqueness => true, :length => {:maximum => 20}
+  validates :email, :username, :teamname, :uniqueness => {:unique => true, :case_sensitive => false}, :length => {:maximum => 20}
   has_many :home_matches, :foreign_key =>  "homeuser_id", :class_name => "Match"
   has_many :away_matches, :foreign_key =>  "awayuser_id", :class_name => "Match"
 
