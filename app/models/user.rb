5c271c50e616b7909ccdd735ccec280fa106118c
oronlavi <oronlavi@gmail.com>
2012-07-19 12:10:21 +0300

    uniqueness of fifa username

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ActiveRecord::Base
   attr_accessible :age, :country, :email, :teamname, :username, :fifa_username
-  validates :email, :username, :teamname, :uniqueness => {:unique => true, :case_sensitive => false}, :length => {:maximum => 20}
+
+  validates :email, :username, :fifa_username, :teamname, :uniqueness =>
+      {:unique => true, :case_sensitive => false}, :length => {:maximum => 20}
+
   has_many :home_matches, :foreign_key => "homeuser_id", :class_name => "Match"
   has_many :away_matches, :foreign_key => "awayuser_id", :class_name => "Match"
 
